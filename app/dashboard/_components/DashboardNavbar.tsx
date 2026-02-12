53c1e2ecb1cc5ffbadde2d9f49c8df81239a8052
kevinforter <49223606+kevinforter@users.noreply.github.com>
2026-02-12 20:08:02 +0100

    changed global stats button to be more responsive

--- app/dashboard/_components/DashboardNavbar.tsx
+++ app/dashboard/_components/DashboardNavbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { BoldCornerButton } from "@/components/BoldCornerButton";
 import { CommandMenu } from "./CommandMenu";
+import { BarChartIcon } from "@radix-ui/react-icons";
 
 interface DashboardNavbarProps {
   countries: any[];
@@ -19,7 +20,7 @@ export function DashboardNavbar({
       <div className="flex items-center gap-4 flex-1">
         <Link href="/" className="group shrink-0">
           <Image
-            src="/icon.svg"
+            src="/oc_logo.svg"
             alt="Open Conflict Logo"
             width={32}
             height={32}
@@ -35,9 +36,10 @@ export function DashboardNavbar({
       </div>
       <BoldCornerButton
         onClick={onGlobalStatsClick}
-        className="shrink-0 text-[10px] ml-2"
+        className="shrink-0 text-[10px] ml-2 flex items-center justify-center"
       >
-        Global Stats
+        <span className="hidden min-[500px]:inline">Global Stats</span>
+        <BarChartIcon className="inline min-[500px]:hidden w-4 h-4 text-white" />
       </BoldCornerButton>
     </nav>
   );
