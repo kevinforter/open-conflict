64d2cd81232774ca235d488dba9cc3c7d9ecbd86
kevinforter <49223606+kevinforter@users.noreply.github.com>
2026-02-13 04:45:25 +0100

    added viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { geistSans, geistMono } from "./fonts/fonts";
 import { CustomCursor } from "@/components/CustomCursor";
@@ -7,6 +7,12 @@ import NavBar from "@/components/NavBar";
 export const metadata: Metadata = {
   title: "Open Conflict",
   description: "Open Data Project by Team Kappa (Ming, Luca and Kevin)",
+};
+
+export const viewport: Viewport = {
+  themeColor: "#101010",
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
